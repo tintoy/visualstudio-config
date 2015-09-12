2206b5b537d7721fb10949988d4c3714f0c422fc
tin toy <tintoy@snoscillator.net>
2015-09-12 10:14:29 +1000

    Split out build process for app vs tests.

--- dist/tests.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var main_1 = require("./main");
-main_1.VisualStudio.loadConfiguration()
-    .then(function (configuration) {
-    console.dir(configuration);
-})
-    .catch(function (error) {
-    console.log("Caught by loadConfiguration callback -", error);
-});
--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,39 @@ var tsc = require("gulp-tsc");
 var mocha = require("gulp-mocha");
 
 var tsConfig = require("./src/tsconfig.json").compilerOptions;
+var testTSConfig = require("./src/tests/tsconfig.json").compilerOptions;
 
 gulp.task("default", ["build", "test"]);
 
-gulp.task("build", function() {
-    return gulp.src("src/**/*.ts")
-        .pipe(
-        	tsc(tsConfig)
-        )
-        .pipe(gulp.dest("dist"));
+gulp.task("build", [
+	"build:app",
+	"build:tests"
+]);
+
+gulp.task("build:app", function() {
+	return gulp.src(["src/**/*.ts", "!src/tests/**/*.ts"])
+		.pipe(
+			tsc(tsConfig)
+		)
+		.pipe(
+			gulp.dest("dist")
+		);
 });
 
-gulp.task("test", ["build"], function() {
-    return gulp.src("dist/tests/**/*.spec.js")
-        .pipe(
-            mocha()
-        );
+gulp.task("build:tests", function() {
+	return gulp.src("src/tests/**/*.ts")
+		.pipe(
+			tsc(testTSConfig)
+		)
+		.pipe(
+			gulp.dest("dist")
+		);
+});
+
+
+gulp.task("test", ["build:tests"], function() {
+	return gulp.src("dist/tests/**/*.spec.js")
+		.pipe(
+			mocha()
+		);
 });
